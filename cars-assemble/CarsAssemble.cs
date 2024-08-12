197d34c676e60300857b6269b427579ec49411c2
Guilherme Ytalo <gydaytalo@gmail.com>
2024-08-12 03:49:24 -0300

    feat: initiated

---
+++ cars-assemble/CarsAssemble.cs
@@ -0,0 +1,19 @@
+using System;
+
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+    }
+    
+    public static double ProductionRatePerHour(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+    }
+}
